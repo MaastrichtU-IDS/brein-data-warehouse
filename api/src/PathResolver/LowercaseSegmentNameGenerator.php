8ff4feab621ae7f0d5fe7e1ee838dc289f919f1c
Michel Dumontier <michel.dumontier@gmail.com>
2020-01-15 13:46:45 +0100

    added lowercase path resolver

---
+++ api/src/PathResolver/LowercaseSegmentNameGenerator.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\PathResolver;
+
+use ApiPlatform\Core\Operation\PathSegmentNameGeneratorInterface;
+
+
+/**
+ * Generate a path name in which the plural form has a _list appended as a suffix.
+ *
+ * @author Michel Dumontier <michel.dumontier@gmail.com>
+ */
+final class LowercaseSegmentNameGenerator implements PathSegmentNameGeneratorInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getSegmentName(string $name, bool $collection = true): string
+    {
+        return strtolower($name);
+    }
+
+}
